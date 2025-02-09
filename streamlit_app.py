1fa3852b597ed5236d957908c8130a59a18d6e9a
V3D4N7 <vedantvinayakghuge@gmail.com>
2025-02-08 22:37:07 -0500

    fixed streamlit chat history.

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,9 +33,11 @@ if True:
         ]
 
     # Display the existing chat messages via `st.chat_message`.
-    # for message in st.session_state.messages:
-    #     with st.chat_message(message["role"]):
-    #         st.markdown(message["content"])
+    for message in st.session_state.messages:
+        if message["role"] == "system":
+            continue
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
